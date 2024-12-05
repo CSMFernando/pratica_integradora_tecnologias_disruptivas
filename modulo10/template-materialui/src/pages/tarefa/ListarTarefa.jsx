9a63e129b9c9466f5c5c6358d9c9f0a512dc7010
CSMFernando <131978616+CSMFernando@users.noreply.github.com>
2024-12-05 15:48:58 -0300

    Update ListarTarefa.jsx
    Alteração das tarefas

--- modulo10/template-materialui/src/pages/tarefa/ListarTarefa.jsx
+++ modulo10/template-materialui/src/pages/tarefa/ListarTarefa.jsx
@@ -33,12 +33,10 @@ function createData(
 
 //Definição do array contendo os dados iniciais da listagem de tarefas
 const initialRows = [
-  createData(1, 'Tarefa 1', 'Descrição da Tarefa 1', '2022-01-01', '2022-01-02', 'Concluída', 'Recurso 1'),
-  createData(2, 'Tarefa 2', 'Descrição da Tarefa 2', '2022-01-03', '2022-01-04', 'Em Andamento', 'Recurso 2'),
-  createData(3, 'Tarefa 3', 'Descrição da Tarefa 3', '2022-01-04', '2022-01-05', 'Em Andamento', 'Recurso 3'),
-  createData(4, 'Tarefa 4', 'Descrição da Tarefa 4', '2022-01-05', '2022-01-06', 'Em Andamento', 'Recurso 4'),
-  createData(5, 'Tarefa 5', 'Descrição da Tarefa 5', '2022-01-06', '2022-01-07', 'Em Andamento', 'Recurso 5'),
-  createData(6, 'Tarefa 6', 'Descrição da Tarefa 6', '2022-01-07', '2022-01-08', 'Aguardando', 'Recurso 6'),
+  createData(1, 'Tarefa 1', 'Manipulação de Template por ReactJS', '2022-09-28', '2024-12-08', 'Aguardando', 'Prática Integradora Tecnologias Disruptivas'),
+  createData(2, 'Tarefa 2', 'Manipulação de Template por Android Studio', '2022-05-14', '2024-04-20', 'Concluída', 'Prática Integradora Desenvolvimento de Apps'),
+  createData(3, 'Tarefa 3', 'Manipulação de Template por Python', '2022-09-28', '2024-06-08', 'Concluída', 'Prática Integradora Desenvolvimento de Software'),
+  createData(4, 'Tarefa 4', 'Manipulação de Template por CLion', '2022-09-28', '2024-10-02', 'Concluída', 'Criação de Aplicações e Sistemas'),
 ];
 
 //Componente ListarTarefa
